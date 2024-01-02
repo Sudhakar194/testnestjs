bb7d097a7ebf0de9ee6c4830a9a383f9e918912a
sudhakar194 <msudhakar086@gmail.com>
2024-01-02 16:40:32 +0300

    static webpage view folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
+// npm install --save @nestjs/serve-static
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..', 'View'),
+      exclude:['/api/(.*)'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
