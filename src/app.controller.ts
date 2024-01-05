6daecd183cb07acb5bec090d52e2db3f4d9cf3dd
sudhakar194 <msudhakar086@gmail.com>
2024-01-05 15:25:06 +0300

    working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,19 +3,26 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('/homepage')
-  gettest(): string {
-    return "[
-      {
-        "item" : "item 1"
-      }
-    ]";
+  resultList = [{
+    itemId: 'item 1',
+    itemName: 'item name 1',
+    itemCode: 'item code 1'
+  },
+  {
+    itemId: 'item 2',
+    itemName: 'item name 2',
+    itemCode: 'item code 2'
+  }];
+
+  @Get('/ResultList')
+  getResultList() {
+    return this.resultList;
   }
 }
