b0dee63f488b6e79710915049530e7dbfcb7c937
unknown <msudhakar086@gmail.com>
2024-01-07 23:05:52 +0300

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
@@ -11,6 +12,20 @@ import { join } from 'path';
     ServeStaticModule.forRoot({ 
       rootPath: join(__dirname,'..', 'View'),
       exclude:['/api/(.*)'],
+    }),
+
+    // to connet mysql database -- npm install --save @nestjs/typeorm typeorm mysql2
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username : 'root',
+      password: 'admin',
+      entities: [],
+      synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+      retryAttempts: 1, //default 10, Number of attempts to connect to the database
+      retryDelay: 1000, //default 3000 , Delay between connection retry attempts (ms) 
+      autoLoadEntities: false // default false , If true, entities will be loaded automatically
     }),
     
   ],
